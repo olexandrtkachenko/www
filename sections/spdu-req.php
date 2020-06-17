205a798c5ae570dc7dba1b11389efa309df80d89
Olexandr Tkachenko <tkachenko.ck@gmail.com>
2020-06-17 12:01:09 +0300

    Create spdu-req.php

---
+++ sections/spdu-req.php
@@ -0,0 +1,73 @@
+<?php $language = wp_cache_get( 'language' ); ?>
+<section class="landing-page-product landing-page-spdu-req">
+  <div class="landing-page-section container people-content-group">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+      <div class="row">
+		  <div class="col-lg-12 col-md-12 col-sm-12" style="
+		      margin-bottom: 4rem;
+		      padding: 0;
+		  "><h2 class="text-uppercase header-dark text-bold primary-header spdu-header" style="
+		      color: #2aabe1;
+		  ">ВИМОГИ</h2></div>
+        <div class="col-lg-6 offset-lg-0 col-md-10 offset-md-1 col-sm-12 req-block">
+          <h2 class="req-title">JAVA</h2>
+          <li class="primary-text text-light">
+			 Java 8 Core: класи/інтерфейси, Generic, Collections API, IO, Exceptions, аннотації;
+          </li>
+          <li class="primary-text text-light">
+			 принципи ООП;
+          </li>
+          <li class="primary-text text-light">
+			 знання алгоритмів і структур даних;
+          </li>
+          <li class="primary-text text-light">
+			 основи SQL;
+          </li>
+          <li class="primary-text text-light">
+			 основи HTML/CSS/JS;
+          </li>
+          <li class="primary-text text-light">
+			 основи Web: HTTP запити, Cookies, Session;
+		  </li>
+          <li class="primary-text text-light">
+			  основи Git;
+          </li>
+          <li class="primary-text text-light">
+			  рівень англійської Intermediate або вище.
+          </li>
+        </div>
+		
+        <div class="col-lg-6 offset-lg-0 col-md-10 offset-md-1 col-sm-12 req-block">
+          <h2 class="req-title">
+			 FRONT-END
+          </h2>
+          <li class="primary-text text-light">
+			  знання основ HTML/CSS;
+          </li>
+          <li class="primary-text text-light">
+			  вміння користуватися одним із графічних редакторів;
+          </li>
+          <li class="primary-text text-light">
+			 досвід верстки від 3 місяців;
+          </li>
+          <li class="primary-text text-light">
+			 досвід Responsive або Adaptive верстки;
+          </li>
+          <li class="primary-text text-light">
+			 розуміння основ програмування, структур даних та алгоритмів;
+          </li>
+          <li class="primary-text text-light">
+			 базові знання Javascript;
+          </li>
+          <li class="primary-text text-light">
+			 основи Git;
+          </li>
+          <li class="primary-text text-light">
+			 рівень англійської Intermediate або вище.
+          </li>
+        </div>
+		
+		
+        
+  </div>
+</section>
