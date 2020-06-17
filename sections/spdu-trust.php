b6b6f4c0dbfb0381e6f1b17627e07d397e226f5f
Olexandr Tkachenko <tkachenko.ck@gmail.com>
2020-06-17 11:25:41 +0300

    Create spdu-trust.php

---
+++ sections/spdu-trust.php
@@ -0,0 +1,34 @@
+<?php $language = wp_cache_get( 'language' ); ?>
+<section class="landing-page-trust">
+  <div class="landing-page-section container snapshot-content-group spdu-trust-block">
+    <div class="col-lg-12 col-md-12 col-sm-12 zero-padding">
+    <h2 class="col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-sm-8 offset-sm-2 text-center">
+      <span class="block-title-thin">ПРОТЯГОМ</span>
+      <span class="block-title-thick">2016-2019 років</span>
+    </h2>
+    <ul class="list-unstyled row trust-list">
+      <li class="trust-list-item col-lg-4 offset-lg-0 col-sm-8 offset-sm-2">
+        <h3 class="trust-list-item-title">
+			110 студентів
+	       <span class="trust-after-title">закінчили</span>
+           <span class="trust-after-title">SPD-University</span>
+        </h3>
+      </li>
+      <li class="trust-list-item col-lg-4 offset-lg-0 col-sm-8 offset-sm-2">
+         <h3 class="trust-list-item-title">
+ 			66% випускників
+ 	       <span class="trust-after-title">приєдналися</span>
+            <span class="trust-after-title">до SPD-Ukraine</span>
+         </h3>
+      </li>
+      <li class="trust-list-item col-lg-4 offset-lg-0 col-sm-8 offset-sm-2">
+         <h3 class="trust-list-item-title">
+ 			80% випускників
+ 	       <span class="trust-after-title">побудували</span>
+            <span class="trust-after-title">кар'єру в IT</span>
+         </h3>
+      </li>
+    </ul>
+    </div>
+  </div>
+</section>
