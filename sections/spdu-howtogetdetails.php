9bce11e31c6fa2acae2da14535f1d43402c2320b
Olexandr Tkachenko <tkachenko.ck@gmail.com>
2020-06-17 11:53:22 +0300

    Create spdu-howtogetdetails.php

---
+++ sections/spdu-howtogetdetails.php
@@ -0,0 +1,30 @@
+<div class="row bgd-light">
+  <div class="container content-group  services-content-group">
+    <div class="col-lg-12 col-md-12 col-sm-12 zero-padding">
+      <h2 class="text-uppercase primary-header header-dark spdu-header">Деталі</h2>
+	  <h3 class="text-uppercase secondary-header spdu-subtext">для того, щоб потрапити до SPD-University потрібно</h3>
+      <p class="primary-text">
+		  Крок перший - зареєструватися на курси на офіційному сайті SPD-University або на сторінці у Facebook. Вам обов‘язково прийде підтвердження про успішну реєстрацію на пошту.
+</p></br>
+
+<p class="primary-text">Другий крок - складання тестів з обраного вами напрямку (англійською мовою). Найкращих за результатами кандидатів ми запросимо на співбесіду.</p></br>
+<p class="primary-text text-bold">Іспит з Java складається з двох етапів:</p>
+<br/>
+<p class="primary-text">Тест з Java 8 Core: 25 питань на 40 хвилин. Питання тесту покривають такі розділи Java: типи даних, розгалудження/цикли, класи/інтерфейси, arrays/Collections, exceptions, основи ООП. Перевіряється автоматично.</p>
+</br>
+<p class="primary-text">Тест з Web: 15 питань на 20 хвилин. Питання тесту покривають основи HTML/CSS/JS, Web tech, SQL та Git. Перевіряється автоматично.</p>
+</br>
+<p class="primary-text text-bold"><a target="_blank" href="https://bit.ly/2JXVRcY">Мінімальні вимоги, ресурси для підготовки та демо-тест</a></p>
+</p>
+</br>
+<p class="primary-text text-bold">Іспит з Front-end складається двох етапів:</p>
+</br>
+<p class="primary-text">Тест з Javascript: 25 питань на 40 хвилин. Питання тесту покривають такі розділи Javascript: variables, statements, data types, numbers and operators, cycles, arrays, events, functions, objects, JS versions (ES5/ES6). Перевіряється автоматично.</p>
+</br>
+<p class="primary-text">Тест з Web UI: 20 питань на 30 хвилин. Питання тесту покривають основи HTML/CSS, основи Web (HTTP запити, cookies, session) та Git. Перевіряється автоматично.</p>
+</br>
+<p class="primary-text text-bold"><a target="_blank" href="https://bit.ly/2SCYe8W">Мінімальні вимоги, ресурси для підготовки та демо-тест</a></p>
+
+    </div>
+  </div>
+</div>
