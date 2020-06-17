91ea06e6c92a9196ce4d4d5c2eadf1f0e795b867
Olexandr Tkachenko <tkachenko.ck@gmail.com>
2020-06-17 11:42:59 +0300

    Create spdu-courses.php

---
+++ sections/spdu-courses.php
@@ -0,0 +1,67 @@
+<section class="landing-page-solutions container landing-page-section zero-padding">
+  <div class="solutions-content-group">
+    <div class="col-lg-12 col-md-12 col-sm-12 zero-padding">
+      <div class="text-center services-header-group">
+        <h2 class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1">
+          <span class="block-title-thin">КУРСИ</span>
+          <span class="block-title-thick">SPD-UNIVERSITY</span>
+        </h2>
+      </div>
+      <div class="d-none d-sm-block">
+        <ul class="list-unstyled solutions-list row">
+          <li class="solutions-item col-lg-6 offset-lg-0 col-sm-8 offset-sm-2">
+            <img src="https://d294zfc0bwifja.cloudfront.net/spdu/images/icons/jfw.svg" alt="Java for Web">
+            <h3>
+	            Java professional
+            </h3>
+            <p class="primary-text">
+				Курс розрахований на тих, хто впевнено володіє Java 8 Core та Web і бажає прокачати на професійному рівні Java стек технологій
+	         </p>
+          </li>
+          <li class="solutions-item col-lg-6 offset-lg-0 col-sm-8 offset-sm-2">
+            <img src="https://d294zfc0bwifja.cloudfront.net/spdu/images/icons/fre.svg" alt="Front-End">
+            <h3>
+	            Front-End
+            </h3>
+            <p class="primary-text">
+			Цей курс призначений для тих, хто хоче навчитися створювати сайти та динамічні веб-додатки</p>
+          </li>
+       
+       </ul>
+      </div>
+      <div class="carousel slide d-block d-sm-none" id="carouselSolutionFirst" data-ride="carousel">
+        <ul class="list-unstyled solutions-list carousel-inner">
+          <li class="solutions-item carousel-item active">
+            <img src="https://d294zfc0bwifja.cloudfront.net/spdu/images/icons/jfw.svg" alt="Java for Web">
+            <h3>
+	            Java professional
+            </h3>
+            <p class="primary-text crs-text">
+				Курс розрахований на тих, хто впевнено володіє Java 8 Core та Web і бажає прокачати на професійному рівні Java стек технологій
+            </p>
+          </li>
+          <li class="solutions-item carousel-item">
+            <img src="https://d294zfc0bwifja.cloudfront.net/spdu/images/icons/fre.svg" alt="Front-End">
+            <h3>
+	            Front-End
+            </h3>
+            <p class="primary-text crs-text">
+				Цей курс призначений для тих, хто хоче навчитися створювати сайти та динамічні веб-додатки</p>
+            </p>
+          </li>
+          
+        </ul>
+        <ol class="carousel-indicators">
+          <li data-target="#carouselSolutionFirst" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselSolutionFirst" data-slide-to="1"></li>
+          <li data-target="#carouselSolutionFirst" data-slide-to="2"></li>
+        </ol>
+      </div>
+      <div class="clearfix btn-wrap">
+        <a href="/courses/" class="btn cta-button button-hover" title="Learn more about our offer" role="button">
+	        Дізнатися більше
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
