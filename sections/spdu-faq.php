a1df3efdc5a425680d2408e3c36ba9d4add65aa3
Olexandr Tkachenko <tkachenko.ck@gmail.com>
2020-06-18 09:28:11 +0300

    Create spdu-faq.php

---
+++ sections/spdu-faq.php
@@ -0,0 +1,69 @@
+<div class="container services-process-block zero-padding">
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+      <h2 class="text-uppercase header-dark text-bold primary-header spdu-header">FAQ</h2>
+      <h3 class="text-uppercase secondary-header services-process-subheader spdu-subtext">У вас виникли питання? Ми з радістю вам допоможемо.</h3>
+    </div>
+    <div class="col-lg-12 col-md-12 col-sm-12">
+      <div class="content-group-column process-content-group-line">
+		  
+          
+          <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+            <div class="process-line-content-b">
+              <h4 class="process-line-header">НАСКІЛЬКИ ЕФЕКТИВНІ КУРСИ??</h4>
+              <p class="primary-text">За чотири роки ми поділилися актуальними знаннями та практичним досвідом з 309 студентами, 141 з яких стали випускниками SPD-University, 59% випускників SPD-Univesity приєднались до команд компанії SPD-Ukraine і 78% випускників стали частиною IT-індустрії.</p>
+            </div>
+          </div>
+		  
+		  
+		  <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+            <div class="process-line-content-b">
+              <h4 class="process-line-header">Скільки коштує навчання в SPD-University?</h4>
+              <p class="primary-text">Навчання на курсах в SPD-University безкоштовне!</p>
+            </div>
+          </div>
+		  
+		  
+  		  <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+            <div class="process-line-content-b">
+              <h4 class="process-line-header">Я ПОДИВИВСЯ ВИМОГИ ДО ОБРАНОГО КУРСУ, АЛЕ УСЕ ЩЕ ВАГАЮСЬ, ЧИ ДОСТАТНІЙ В МЕНЕ РІВЕНЬ. ЧИ Є МОЖЛИВІСТЬ ПРОКОНСУЛЬТУВАТИСЯ?</h4>
+              <p class="primary-text">Так, пишіть на <a target="_blank" href="mailto:info@spd-university.com">пошту</a> або на<a target="_blank" href="https://www.facebook.com/SPD.University/"> Facebook сторінці</a> </p>
+            </div>
+          </div>
+		  
+		  
+
+		  <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+          <div class="process-line-content-b">
+            <h4 class="process-line-header">Де проходитимуть заняття на курсах SPD-University?</h4>
+            <p class="primary-text">Заняття проходитимуть в нашому офісі за адресою вулиця Кривалівська (Паризької комуни) 7, 5-й поверх</p>
+          </div>
+        </div>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+          <div class="process-line-content-b">
+            <h4 class="process-line-header">Яка тривалість курсу?</h4>
+            <p class="primary-text">Загальна тривалість курсу 7 місяців, 5 з яких - заняття з викладачами, лекції та практичні заняття, та ще 1,5 місяці на розробку фінального проекту в командах.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+          <div class="process-line-content-b">
+            <h4 class="process-line-header">Чи можна суміщати роботу/навчання з курсами SPD-University?</h4>
+            <p class="primary-text">Звичайно. Ми чудово розуміємо, що студенти наших курсів можуть навчатися у вищі або ж працювати, тому для зручності заняття в SPD-University розпочинаються о 18:00-18:30.</p>
+          </div>
+        </div>
+		
+        
+        <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+          <div class="process-line-content-b">
+            <h4 class="process-line-header">Я ПРОХОДИВ НАВЧАННЯ В SPD-UNIVERSITY В МИНУЛИХ СЕЗОНАХ, АЛЕ НЕ ДІЙШОВ ДО ФІНІШУ. ЧИ Є У МЕНЕ МОЖЛИВІСТЬ СПРОБУВАТИ ЩЕ РАЗ?</h4>
+            <p class="primary-text">Так, така можливість існує.</p>
+          </div>
+        </div>
+		
+		
+      </div>
+    </div>
+  </div>
+</div>
