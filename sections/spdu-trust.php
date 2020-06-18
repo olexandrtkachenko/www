1cc2a5e7c90e34451d02e0c4c5949ad8fc50d391
Olexandr Tkachenko <tkachenko.ck@gmail.com>
2020-06-18 09:21:53 +0300

    Update spdu-trust.php

--- sections/spdu-trust.php
+++ sections/spdu-trust.php
@@ -4,26 +4,26 @@
     <div class="col-lg-12 col-md-12 col-sm-12 zero-padding">
     <h2 class="col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-sm-8 offset-sm-2 text-center">
       <span class="block-title-thin">ПРОТЯГОМ</span>
-      <span class="block-title-thick">2016-2019 років</span>
+      <span class="block-title-thick">2016-2020 років</span>
     </h2>
     <ul class="list-unstyled row trust-list">
       <li class="trust-list-item col-lg-4 offset-lg-0 col-sm-8 offset-sm-2">
         <h3 class="trust-list-item-title">
-			110 студентів
+			141 студентів
 	       <span class="trust-after-title">закінчили</span>
            <span class="trust-after-title">SPD-University</span>
         </h3>
       </li>
       <li class="trust-list-item col-lg-4 offset-lg-0 col-sm-8 offset-sm-2">
          <h3 class="trust-list-item-title">
- 			66% випускників
+ 			59% випускників
  	       <span class="trust-after-title">приєдналися</span>
             <span class="trust-after-title">до SPD-Ukraine</span>
          </h3>
       </li>
       <li class="trust-list-item col-lg-4 offset-lg-0 col-sm-8 offset-sm-2">
          <h3 class="trust-list-item-title">
- 			80% випускників
+ 			78% випускників
  	       <span class="trust-after-title">побудували</span>
             <span class="trust-after-title">кар'єру в IT</span>
          </h3>
