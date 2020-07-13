8552ed21bcc70e0e9b2a7a299d1ad4b24e724682
Olexandr Tkachenko <tkachenko.ck@gmail.com>
2020-07-13 18:59:03 +0300

    Update spdu-faq.php

--- sections/spdu-faq.php
+++ sections/spdu-faq.php
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
       <h2 class="text-uppercase header-dark text-bold primary-header spdu-header">FAQ</h2>
-      <h3 class="text-uppercase secondary-header services-process-subheader spdu-subtext">У вас виникли питання? Ми з радістю вам допоможемо.</h3>
+      <h3 class="text-uppercase secondary-header services-process-subheader spdu-subtext">ВИНИКЛИ ПИТАННЯ? МИ З РАДІСТЮ ДОПОМОЖЕМО.</h3>
     </div>
     <div class="col-lg-12 col-md-12 col-sm-12">
       <div class="content-group-column process-content-group-line">
@@ -11,7 +11,7 @@
           <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
             <div class="process-line-content-b">
               <h4 class="process-line-header">НАСКІЛЬКИ ЕФЕКТИВНІ КУРСИ??</h4>
-              <p class="primary-text">За чотири роки ми поділилися актуальними знаннями та практичним досвідом з 309 студентами, 141 з яких стали випускниками SPD-University, 59% випускників SPD-Univesity приєднались до команд компанії SPD-Ukraine і 78% випускників стали частиною IT-індустрії.</p>
+              <p class="primary-text">З 2016 року ми поділилися актуальними знаннями та практичним досвідом з 309 студентами, 141 з яких отримали диплом випускника SPD-University. 60% випускників SPD-University приєднались до SPD-Ukraine. 80% випускників стали частиною IT-індустрії.</p>
             </div>
           </div>
 		  
@@ -19,7 +19,7 @@
 		  <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
             <div class="process-line-content-b">
               <h4 class="process-line-header">Скільки коштує навчання в SPD-University?</h4>
-              <p class="primary-text">Навчання на курсах в SPD-University безкоштовне!</p>
+              <p class="primary-text">Навчання безкоштовне!</p>
             </div>
           </div>
 		  
@@ -27,7 +27,7 @@
   		  <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
             <div class="process-line-content-b">
               <h4 class="process-line-header">Я ПОДИВИВСЯ ВИМОГИ ДО ОБРАНОГО КУРСУ, АЛЕ УСЕ ЩЕ ВАГАЮСЬ, ЧИ ДОСТАТНІЙ В МЕНЕ РІВЕНЬ. ЧИ Є МОЖЛИВІСТЬ ПРОКОНСУЛЬТУВАТИСЯ?</h4>
-              <p class="primary-text">Так, пишіть на <a target="_blank" href="mailto:info@spd-university.com">пошту</a> або на<a target="_blank" href="https://www.facebook.com/SPD.University/"> Facebook сторінці</a> </p>
+              <p class="primary-text">Так, пиши нам на <a target="_blank" href="mailto:info@spd-university.com">пошту</a> або на<a target="_blank" href="https://www.facebook.com/SPD.University/"> Facebook сторінці</a> </p>
             </div>
           </div>
 		  
@@ -35,15 +35,15 @@
 
 		  <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
           <div class="process-line-content-b">
-            <h4 class="process-line-header">Де проходитимуть заняття на курсах SPD-University?</h4>
-            <p class="primary-text">Заняття проходитимуть в нашому офісі за адресою вулиця Кривалівська (Паризької комуни) 7, 5-й поверх</p>
+            <h4 class="process-line-header">ДДЕ ПРОХОДИТИМУТЬ ЗАНЯТТЯ НА КУРСАХ SPD-UNIVERSITY?</h4>
+            <p class="primary-text">Заняття проходитимуть переважно в дистанційному форматі (згідно розкладу занять), а також в нашій лекційній аудиторії за адресою м. Черкаси, вул. Кривалівська (П. Комуни) 7.</p>
           </div>
         </div>
 
         <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
           <div class="process-line-content-b">
             <h4 class="process-line-header">Яка тривалість курсу?</h4>
-            <p class="primary-text">Загальна тривалість курсу 7 місяців, 5 з яких - заняття з викладачами, лекції та практичні заняття, та ще 1,5 місяці на розробку фінального проекту в командах.</p>
+            <p class="primary-text">Тривалість основних курсів Front-end та Java (for Web) — 5 місяців (жовтень — лютий). В разі успішного завершення курсу ти отримаєш сертифікат SPD-University.</p>
           </div>
         </div>
 
@@ -54,6 +54,12 @@
           </div>
         </div>
 		
+        <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+          <div class="process-line-content-b">
+            <h4 class="process-line-header">ЩО ТАКЕ INTERNSHIP І ЯК ДО НЕЇ ПОТРАПИТИ?</h4>
+            <p class="primary-text">SPD-University Internship - це 2 місяці (березень-квітень) збалансованої практики та роботи над проектами під керівництвом досвідченого PM та команди менторів, в умовах, що максимально наближені до реальних за напрямком QA, Front-end (React/Angular), Java. Навчання відбувається з пн-пт 18:00 до 21:00. По завершенню - сертифікат SPD-University.</p>
+          </div>
+        </div>
         
         <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
           <div class="process-line-content-b">
