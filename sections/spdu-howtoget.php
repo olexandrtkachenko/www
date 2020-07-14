9c717554e09221dbae486622c33bcd64ed66a83b
Olexandr Tkachenko <tkachenko.ck@gmail.com>
2020-07-14 08:25:02 +0300

    Create spdu-howtoget.php

---
+++ sections/spdu-howtoget.php
@@ -0,0 +1,36 @@
+<div class="container services-process-block zero-padding">
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+      <h2 class="text-uppercase header-dark text-bold primary-header spdu-header">ЯК ПОТРАПИТИ?</h2>
+      <h3 class="text-uppercase secondary-header services-process-subheader spdu-subtext">ЧОТИРИ КРОКИ, І ТИ НА  КУРСІ В SPD-UNIVERSITY</h3>
+    </div>
+    <div class="col-lg-12 col-md-12 col-sm-12">
+      <div class="content-group-column process-content-group-line">
+		          <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+          <div class="process-line-content-b">
+            <h4 class="process-line-header">РЕЄСТРАЦІЯ</h4>
+            <p class="primary-text">Зареєструйся на нашому сайті або за посиланням у FB</p>
+          </div>
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+          <div class="process-line-content-b">
+            <h4 class="process-line-header">ТЕСТУВАННЯ</h4>
+            <p class="primary-text">Пройди тестування (англійською мовою) з напрямку, який ти обрав</p>
+          </div>
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+          <div class="process-line-content-b">
+            <h4 class="process-line-header">СПІВБЕСІДА</h4>
+            <p class="primary-text">Якщо успішного склав тест - пройди технічну співбесіду</p>
+          </div>
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 process-line-element">
+          <div class="process-line-content-b">
+            <h4 class="process-line-header">ЗАПРОШЕННЯ</h4>
+            <p class="primary-text">Отримай запрошення на навчання та ставай студентом SPD-University!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
