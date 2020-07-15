5d5f994c10a75806fd0bf7bd3790a38368477959
Olexandr Tkachenko <tkachenko.ck@gmail.com>
2020-07-15 14:50:35 +0300

    Create spdu-team.php

---
+++ sections/spdu-team.php
@@ -0,0 +1,206 @@
+<div class="container hide-overflow">
+  <div class="row leadership-row">
+    <div class="container content-group">
+      <div class="col-lg-12 col-md-12 col-sm-12 leadership-content">
+        <h2 class="text-uppercase primary-header header-dark spdu-header">КОМАНДА</h2>
+        <h3 class="text-uppercase secondary-header spdu-subtext">Практикуючі професіонали, які створюють професіоналів майбутнього</h3>
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+			                     src="https://d294zfc0bwifja.cloudfront.net/spdu/images/o_n.png" alt="Олег Новіцький"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Олег Новіцький</b></p>
+                <p>Oracle Certified Professional Java 8 Programmer, 15 років досвіду</p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/l_b.png" alt="Леонід Байда"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Леонід Байда</b></p>
+                <p>Front-end Team Lead / Senior Front-end Developer, 13 років досвіду</p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/s_v.png" alt="Станіслав Висоцький"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Станіслав Висоцький</b></p>
+                <p>Front-end Team Lead / Senior Software Engineer, 6 років досвіду</p>
+              </div>
+            </div>
+          </div>
+		  
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/i_d.png" alt="Ігор Доліч<"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Ігор Доліч</b></p>
+                <p>Front-end Team Lead / Senior Front-end Developer, 8 років досвіду</p>
+              </div>
+            </div>
+          </div>
+		  
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/v_m.png" alt="Владислав Мусієнко"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Владислав Мусієнко</b></p>
+                <p>Front-end Team Lead / Senior Front-end Developer, 5 років досвіду</p>
+              </div>
+            </div>
+          </div>
+		  		  
+		  
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/v_s.png" alt="Володимир Шевченко"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Володимир Шевченко</b></p>
+                <p>Java Team Lead, 9 років досвіду</p>
+              </div>
+            </div>
+          </div>
+		  
+		  
+
+		  
+
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/a_z.png" alt="Андрій Заяць"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Андрій Заяць</b></p>
+                <p>Java developer, Oracle Certified Professional Java 8 Programmer, 6 років досвіду</p>
+              </div>
+            </div>
+          </div>
+
+
+ 		 <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+			 <div class="text-center">
+			 <img class="img-full-width img-fluid" src="https://d294zfc0bwifja.cloudfront.net/spdu/images/r_p.png" alt="Руслан Пістряк"/>
+			 <div class="our-leaders-content-group-name primary-text">
+				 <p><b>Руслан Пістряк</b></p>
+				 <p>Java Team Lead, Oracle Certified Professional Java 11 Programmer, 5 років досвіду</p>
+			 </div>
+		 </div>
+	 </div>
+
+
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/s_r.png" alt="Сергій Рибалко"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Сергій Рибалко</b></p>
+                <p>Java developer, Oracle Certified Professional Java 8 Programmer, 12 років досвіду</p>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/a_d.png" alt="Анна Дума"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Анна Дума</b></p>
+                <p>Project Manager</p>
+              </div>
+            </div>
+          </div>
+		  
+		  
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/o_d.png" alt="Олександр Динда"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Олександр Динда</b></p>
+                <p>Project Manager</p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/o_k.png" alt="Оксана Кіржбаум"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Оксана Кіржбаум</b></p>
+                <p>Project Manager</p>
+              </div>
+            </div>
+          </div>
+		  
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/a_s.png" alt="Анастасія Сергієнко"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p class="text-capitalize"><b>Анастасія Сергієнко</b></p>
+                <p>QA Lead</p>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/d_s.png" alt="Дмитро Шевченко"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Дмитро Шевченко</b></p>
+                <p>Java Developer, 2 роки досвіду</p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/d_sh.png" alt="Дмитро Шаповалов"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Дмитро Шаповалов</b></p>
+                <p>Front-end Developer, 2 роки досвіду</p>
+              </div>
+            </div>
+          </div>
+		  
+		  
+          <div class="col-lg-3 col-md-6 col-sm-6 our-leaders-content-group-img">
+            <div class="text-center">
+              <img class="img-full-width img-fluid"
+                   src="https://d294zfc0bwifja.cloudfront.net/spdu/images/a_t.png" alt="Аліна Тернова"/>
+              <div class="our-leaders-content-group-name primary-text">
+                <p><b>Аліна Тернова</b></p>
+                <p>Quality Assurance Engineer, ISTQB® Test Analyst Advanced Level, 6 років досвіду</p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
